00cf3325a3d0558299397f71e0a49b964d1aef54
Samuel Tupa Febrian <samuel.febrian@gmail.com>
2018-05-10 20:43:09 +0700

    Fix import order issue (#6)

--- src/main/java/advprog/oriconbluray/util/RankScrapper.java
+++ src/main/java/advprog/oriconbluray/util/RankScrapper.java
@@ -1,13 +1,13 @@
 package advprog.oriconbluray.util;
 
+import java.io.IOException;
+import java.util.stream.Collectors;
+
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.util.stream.Collectors;
 
 public class RankScrapper {
 
