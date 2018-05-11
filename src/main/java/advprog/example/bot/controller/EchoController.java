d3c8f85f2f3dc26fcd6ad6c70bc3f06b4e053015
Norman <normanbintang@hotmail.co.id>
2018-05-11 16:28:43 +0700

    fix style issue

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -16,6 +16,7 @@ public class EchoController {
 
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+
         LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
                 event.getTimestamp(), event.getMessage()));
         TextMessageContent content = event.getMessage();
--- src/test/java/advprog/example/bot/controller/EchoControllerTest.java
+++ src/test/java/advprog/example/bot/controller/EchoControllerTest.java
@@ -58,10 +58,10 @@ public class EchoControllerTest {
     }
 
     @Test
-    void testHandleDocumentsSimilarityURLCheck() {
+    void testHandleDocumentsSimilarityUrlCheck() {
         MessageEvent<TextMessageContent> event =
-                EventTestUtil.createDummyTextMessage("/docs_sim https://www.biography.com/people/adolf-hitler-9340144" +
-                        "https://en.wikipedia.org/wiki/Adolf_Hitler");
+                EventTestUtil.createDummyTextMessage("/docs_sim https://www.biography.com/people/adolf-hitler-9340144"
+                        + "https://en.wikipedia.org/wiki/Adolf_Hitler");
 
         TextMessage reply = echoController.handleTextMessageEvent(event);
         assertEquals("87%", reply.getText());
