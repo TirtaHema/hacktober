b23283b96b32e03956d03f3e8f69607c22ee6d54
Norman <normanbintang@hotmail.co.id>
2018-05-12 01:26:36 +0700

    Change helper return value from double to string

--- src/test/java/advprog/example/bot/service/DocumentsSimilarityHelperTest.java
+++ src/test/java/advprog/example/bot/service/DocumentsSimilarityHelperTest.java
@@ -1,12 +1,9 @@
 package advprog.example.bot.service;
 
-import org.assertj.core.api.Assertions;
-import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DocumentsSimilarityHelperTest {
 
@@ -19,36 +16,45 @@ public class DocumentsSimilarityHelperTest {
 
     @Test
     public void testGetSimilarity1() {
-        double similarity = documentsSimilarityHelper.getSimilarity(
+        String similarity = documentsSimilarityHelper.getSimilarity(
                 "'my name is fahmi' 'your name is fahmi'"
         );
-        assertEquals(1, similarity, 1e-2);
+        assertEquals("100%", similarity);
     }
 
     @Test
     public void testGetSimilarity2() {
-        double similarity = documentsSimilarityHelper.getSimilarity(
+        String similarity = documentsSimilarityHelper.getSimilarity(
                 "https://www.biography.com/people/adolf-hitler-9340144 "
                 + "https://en.wikipedia.org/wiki/Adolf_Hitler"
         );
-        assertEquals(0.87, similarity, 1e-2);
+        assertEquals("87%", similarity);
     }
 
     @Test
     public void testGetSimilarityFromText() {
-        double similarity = documentsSimilarityHelper.getSimilarityFromText(
+        String similarity = documentsSimilarityHelper.getSimilarityFromText(
                 "my name is fahmi",
                 "your name is fahmi"
         );
-        assertEquals(1, similarity, 1e-2);
+        assertEquals("100%", similarity);
+    }
+
+    @Test
+    public void testGetSimilarityFromText2() {
+        String similarity = documentsSimilarityHelper.getSimilarityFromText(
+                "hey ho",
+                "ho hey"
+        );
+        assertEquals("Unmanaged language [so]", similarity);
     }
 
     @Test
     public void testGetSimilarityFromUrl() {
-        double similarity = documentsSimilarityHelper.getSimilarityFromUrl(
+        String similarity = documentsSimilarityHelper.getSimilarityFromUrl(
                 "https://www.biography.com/people/adolf-hitler-9340144",
                 "https://en.wikipedia.org/wiki/Adolf_Hitler"
         );
-        assertEquals(0.87, similarity, 1e-2);
+        assertEquals("87%", similarity);
     }
 }
