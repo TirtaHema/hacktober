5933d03427ffa63e84708c7bf9b0a8895ebdddbb
Samuel Tupa Febrian <samuel.febrian@gmail.com>
2018-05-11 18:16:12 +0700

    Create rank command's configuration stubs (#6)

---
+++ src/main/java/advprog/oriconbluray/util/commands/config/DailyRankCommandConfig.java
@@ -0,0 +1,10 @@
+package advprog.oriconbluray.util.commands.config;
+
+import advprog.oriconbluray.util.commands.impls.DailyRankCommand;
+
+public class DailyRankCommandConfig {
+
+    DailyRankCommand dailyRankCommand() {
+        return null;
+    }
+}
---
+++ src/main/java/advprog/oriconbluray/util/commands/config/WeeklyRankCommandConfig.java
@@ -0,0 +1,10 @@
+package advprog.oriconbluray.util.commands.config;
+
+import advprog.oriconbluray.util.commands.impls.WeeklyRankCommand;
+
+public class WeeklyRankCommandConfig {
+
+    WeeklyRankCommand weeklyRankCommand() {
+        return null;
+    }
+}
