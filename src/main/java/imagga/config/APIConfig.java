97317f9148fb230a39882cf7aec4e507e42f62ef
Mohammad Ammar Ramadhan <mohammad.ammar61@ui.ac.id>
2018-05-11 20:45:07 +0700

    Create stub API Imagga (#15)

---
+++ src/main/java/imagga/config/APIConfig.java
@@ -0,0 +1,28 @@
+package imagga.config;
+
+import java.net.URL;
+
+public class APIConfig {
+
+    private String key;
+    private String secret;
+    private final URL urlObject = new URL("https://api.imagga.com/v1");
+    private String endpoint;
+
+    public APIConfig(String key, String secret){
+        this.key = key;
+        this.secret = secret;
+    }
+
+    public String setEndpoint(String endpoint){
+        this.endpoint = endpoint;
+    }
+
+    public String getEndpointUrl(){
+        return urlObject+endpoint;
+    }
+    public String getAuth(){
+        /*TBD*/
+    }
+
+}
