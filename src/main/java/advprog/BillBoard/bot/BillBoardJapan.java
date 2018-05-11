e878bfd6c6e9ba1a9e4e1175ee7fbec777344307
regensa18 <regensa.cahyanto18@gmail.com>
2018-05-11 17:23:59 +0700

    added BillboardArtist200Controller and BillboardArtist200ControllerTest

--- src/main/java/advprog/BillBoard/bot/BillBoardJapan.java
+++
@@ -1,9 +0,0 @@
-package advprog.BillBoard.bot;
-
-import java.util.logging.Logger;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-public class BillBoardJapan {
-}
