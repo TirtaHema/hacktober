61860440281946e3fdd652ba28f8acb39ecb3a8e
Samuel Tupa Febrian <samuel.febrian@gmail.com>
2018-05-11 11:15:22 +0700

    Create RankCommandControl stub as guide to create the test (#6)

---
+++ src/main/java/advprog/oriconbluray/util/commands/RankCommandControl.java
@@ -0,0 +1,25 @@
+package advprog.oriconbluray.util.commands;
+
+import com.linecorp.bot.model.message.TextMessage;
+
+import java.util.Map;
+
+public class RankCommandControl {
+
+    private Map<String, RankCommand> commandMap;
+
+    public void addCommand(String key, RankCommand command) {
+
+    }
+
+    public void removeCommand(String key) {
+
+    }
+
+    public TextMessage execute(String key) {
+        return null;
+    }
+    private void registerCommands() {
+
+    }
+}
