1c97371f51060e5d9a6db1d46a24ab58f0852517
Windi Chandra <windi.chandra@gmail.com>
2018-05-11 20:36:49 +0700

    [WIP] create API Helper and partial service implementation

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -1,5 +1,7 @@
 package advprog.example.bot.controller;
 
+import advprog.example.bot.helper.Album;
+import advprog.example.bot.helper.MusicBrainzAPIHelper;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
@@ -7,12 +9,14 @@ import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 @LineMessageHandler
 public class EchoController {
 
     private static final Logger LOGGER = Logger.getLogger(EchoController.class.getName());
+    private static final MusicBrainzAPIHelper musicBrainzAPIHelper = new MusicBrainzAPIHelper();
 
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
@@ -36,7 +40,25 @@ public class EchoController {
     }
 
     public String queryArtistRecentAlbum(String artist) {
-        return "Artist not found";
+        try {
+            List<Album> albumList = musicBrainzAPIHelper.getMostRecentAlbumByArtistID(musicBrainzAPIHelper.getArtistId(artist));
+            StringBuilder result = new StringBuilder();
+
+            result.append(artist);
+            result.append("\n");
+            for (Album album : albumList) {
+                result.append(album.getDate());
+                result.append(" ");
+                result.append(album.getName());
+                result.append("\n");
+            }
+
+            return result.toString();
+        } catch (Exception exception) {
+            LOGGER.fine(exception.toString());
+
+            return null;
+        }
     }
 
 }
