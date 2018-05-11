85469a80846db5b3a8a316c187bf669b6b705ae6
Samuel Tupa Febrian <samuel.febrian@gmail.com>
2018-05-11 08:29:15 +0700

    Implement interface to ease the command management (#6)

---
+++ src/main/java/advprog/oriconbluray/util/commands/interfaces/RankCommand.java
@@ -0,0 +1,7 @@
+package advprog.oriconbluray.util.commands.interfaces;
+
+import com.linecorp.bot.model.message.TextMessage;
+
+public interface RankCommand {
+    public TextMessage execute(String date);
+}
