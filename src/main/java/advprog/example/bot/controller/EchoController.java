d1c3a8e60807f1fa8431b41dda22e0bd20cc9aa7
Kautsar Fadlillah <kautsarfadlillah@gmail.com>
2018-05-11 18:26:43 +0700

    Write Stubs (#31)

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -22,13 +22,39 @@ public class EchoController {
         TextMessageContent content = event.getMessage();
         String contentText = content.getText();
 
-        String replyText = contentText.replace("/echo", "");
-        return new TextMessage(replyText.substring(1));
+        TextMessage replayMessage;
+
+        switch (contentText.split(" ")[0]) {
+            case "/echo" : {
+                replayMessage = new TextMessage(contentText.replace("/echo", "").substring(1));
+                break;
+            }
+            case "/toplaughers" : {
+                replayMessage = handleTopLaughers(event);
+                break;
+            }
+            default: {
+                replayMessage = new TextMessage(contentText);
+                break;
+            }
+        }
+
+        return replayMessage;
     }
 
     @EventMapping
     public void handleDefaultMessage(Event event) {
         LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
                 event.getTimestamp(), event.getSource()));
+    }
+
+    public TextMessage handleTopLaughers(Event event) {
+        String message = "1. \n" +
+                "2. \n" +
+                "3. \n" +
+                "4. \n" +
+                "5. \n";
+
+        return new TextMessage(message);
     }
 }
