6d771de9961a859203e26e5e46c6ef827d8aa900
regensa18 <regensa.cahyanto18@gmail.com>
2018-05-11 11:44:10 +0700

    added file BillBoardJapan

---
+++ src/main/java/advprog/BillBoard/bot/BillBoardJapan.java
@@ -0,0 +1,9 @@
+package advprog.BillBoard.bot;
+
+import java.util.logging.Logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class BillBoardJapan {
+}
