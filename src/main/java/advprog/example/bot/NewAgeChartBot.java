934bc30996bf60426aa8923e97e7c64e7f2e7047
Wildan Fahmi <wildan.fahmi@ui.ac.id>
2018-05-11 18:52:48 +0700

    added method

--- src/main/java/advprog/example/bot/NewAgeChartBot.java
+++ src/main/java/advprog/example/bot/NewAgeChartBot.java
@@ -1,4 +1,14 @@
 package advprog.example.bot;
 
+import org.springframework.boot.SpringApplication;
+
+import java.util.logging.Logger;
+
 public class NewAgeChartBot {
+    private static final Logger LOGGER = Logger.getLogger(BotExampleApplication.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.info("Application starting ...");
+        SpringApplication.run(BotExampleApplication.class, args);
+    }
 }
