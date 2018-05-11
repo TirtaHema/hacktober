8194eacd15cf717596428c5722a61575be826188
Norman <normanbintang@hotmail.co.id>
2018-05-11 23:21:27 +0700

    create stub and test for documents similarity helper

---
+++ src/main/java/advprog/example/bot/service/DocumentsSimilarityHelper.java
@@ -0,0 +1,17 @@
+package advprog.example.bot.service;
+
+public class DocumentsSimilarityHelper {
+    private static final String API_URL = "https://api.dandelion.eu/datatxt/sim/v1/";
+
+    public double getSimilarityFromText(String text1, String text2) {
+        return 0;
+    }
+
+    public double getSimilarityFromUrl(String url1, String url2) {
+        return 0;
+    }
+
+    public double getSimilarity(String content) {
+        return 0;
+    }
+}
---
+++ src/test/java/advprog/example/bot/service/DocumentsSimilarityHelperTest.java
@@ -0,0 +1,45 @@
+package advprog.example.bot.service;
+
+import org.assertj.core.api.Assertions;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DocumentsSimilarityHelperTest {
+
+    private DocumentsSimilarityHelper documentsSimilarityHelper;
+
+    @BeforeEach
+    public void setUp() {
+        documentsSimilarityHelper = new DocumentsSimilarityHelper();
+    }
+
+    @Test
+    public void testGetSimilarity() {
+        double similarity = documentsSimilarityHelper.getSimilarity(
+                "'my name is fahmi' 'your name is fahmi'"
+        );
+        assertEquals(1, similarity, 1e-9);
+    }
+
+    @Test
+    public void testGetSimilarityFromText() {
+        double similarity = documentsSimilarityHelper.getSimilarityFromText(
+                "my name is fahmi",
+                "your name is fahmi"
+        );
+        assertEquals(1, similarity, 1e-9);
+    }
+
+    @Test
+    public void testGetSimilarityFromUrl() {
+        double similarity = documentsSimilarityHelper.getSimilarityFromUrl(
+                "https://www.biography.com/people/adolf-hitler-9340144",
+                "https://en.wikipedia.org/wiki/Adolf_Hitler"
+        );
+        assertEquals(0.87, similarity, 1e-9);
+    }
+}
