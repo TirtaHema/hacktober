5960f95daef1a31d16d94bd15c5b0018628c5c5f
Samuel Tupa Febrian <samuel.febrian@gmail.com>
2018-05-11 09:28:01 +0700

    Fxing checkstyle on main (#6)

--- src/main/java/advprog/oriconbluray/util/commands/DailyRankCommand.java
+++ src/main/java/advprog/oriconbluray/util/commands/DailyRankCommand.java
@@ -2,7 +2,6 @@ package advprog.oriconbluray.util.commands;
 
 import advprog.oriconbluray.util.service.rankscrapper.RankScrapper;
 import com.linecorp.bot.model.message.TextMessage;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
 
@@ -10,16 +9,16 @@ public class DailyRankCommand implements RankCommand {
 
     private RankScrapper rankScrapper = new RankScrapper();
 
-    private final String DAILY_URL = "https://www.oricon.co.jp/rank/bd/d/";
+    private static final String DAILY_URL = "https://www.oricon.co.jp/rank/bd/d/";
 
 
     @Override
-    public TextMessage execute(String date) throws IOException{
+    public TextMessage execute(String date) throws IOException {
         String rankList = rankScrapper
                 .scrapRank(DAILY_URL + date + "/");
 
-        rankList = rankList.contains("Not a valid URL") ?
-                "It seems there's no rank charts on that date"
+        rankList = rankList.contains("Not a valid URL")
+                ? "It seems there's no rank charts on that date"
                 : rankList;
         return new TextMessage(rankList);
     }
--- src/main/java/advprog/oriconbluray/util/commands/WeeklyRankCommand.java
+++ src/main/java/advprog/oriconbluray/util/commands/WeeklyRankCommand.java
@@ -9,15 +9,15 @@ public class WeeklyRankCommand implements RankCommand {
 
     private RankScrapper rankScrapper = new RankScrapper();
 
-    private final String WEEKLY_URL = "https://www.oricon.co.jp/rank/bd/w/";
+    private static final String WEEKLY_URL = "https://www.oricon.co.jp/rank/bd/w/";
 
     @Override
     public TextMessage execute(String date) throws IOException {
         String rankList = rankScrapper
                 .scrapRank(WEEKLY_URL + date + "/");
 
-        rankList = rankList.contains("Not a valid URL") ?
-                "It seems there's no rank charts on that date"
+        rankList = rankList.contains("Not a valid URL")
+                ? "It seems there's no rank charts on that date"
                 + ". Make sure the date you assign is on monday"
                 : rankList;
         return new TextMessage(rankList);
