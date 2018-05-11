c01babc1c47fbfac28b5ca32c42ce3e772440a97
Justin Sung <e.justin.sung@gmail.com>
2018-05-11 19:03:21 +0700

    first commit

--- src/main/java/advprog/example/bot/BotRelevantNewsApplication.java
+++ src/main/java/advprog/example/bot/BotRelevantNewsApplication.java
@@ -6,12 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BotExampleApplication {
+public class BotRelevantNewsApplication {
 
-    private static final Logger LOGGER = Logger.getLogger(BotExampleApplication.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(BotRelevantNewsApplication.class.getName());
 
     public static void main(String[] args) {
         LOGGER.info("Application starting ...");
-        SpringApplication.run(BotExampleApplication.class, args);
+        SpringApplication.run(BotRelevantNewsApplication.class, args);
     }
 }
