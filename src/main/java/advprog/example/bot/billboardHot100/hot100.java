8363fe833a314f6e44ef42ef9e448ee0432b6d94
Marco Kenata <marco.kenata@ui.ac.id>
2018-05-11 17:24:05 +0700

    fully functional script + >70% code coverage

--- src/main/java/advprog/example/bot/billboardHot100/hot100.java
+++ src/main/java/advprog/example/bot/billboardHot100/hot100.java
@@ -1,8 +1,36 @@
 package advprog.example.bot.billboardhot100;
 
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+
 public class Hot100 {
+
     public String getChart() {
-        return "";
+        String concate = "";
+        try {
+            String url = "https://www.billboard.com/charts/hot-100";
+            Document document = Jsoup.connect(url).timeout(5000).get();
+            Elements link = document.getElementsByClass("chart-row");
+
+            for (int i = 0; i < 10; i++) {
+                concate += "(" + (i+1) + ") ";
+                concate += link.get(i).getElementsByClass("chart-row__song").html()
+                        .replaceAll("&amp;","&") + " - ";
+                concate += link.get(i).getElementsByClass("chart-row__artist").html()
+                        .replaceAll("&amp;","&") + "\n";
+            }
+
+        } catch (SocketTimeoutException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return concate;
     }
 }
