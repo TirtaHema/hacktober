778f0ae9785e6519c5610b286be9c9e3f2cd393a
Marco Kenata <marco.kenata@ui.ac.id>
2018-05-11 10:34:12 +0700

    first commit

---
+++ src/main/java/advprog/example/bot/billboardHot100/hot100.java
@@ -0,0 +1,8 @@
+package advprog.example.bot.billboardHot100;
+
+
+public class hot100 {
+    public String GetChart()  {
+        return "";
+    }
+}
---
+++ src/test/java/advprog/example/bot/billboardHot100/hot100Test.java
@@ -0,0 +1,26 @@
+package advprog.example.bot.billboardHot100;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class hot100Test {
+
+    private hot100 hot100;
+
+    @Before
+    public void setUp(){
+        hot100 = new hot100();
+    }
+
+    @Test
+    public void Test(){
+        String charts = hot100.GetChart();
+        assertNotNull(charts);
+    }
+
+    
+}
