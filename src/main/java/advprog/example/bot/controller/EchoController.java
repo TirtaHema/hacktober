3c0f3fe1faddfa95b395b77235236f625de27464
Yusuf <yfsholeh@gmail.com>
2018-05-12 23:55:50 +0700

    add mock class and method

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -235,13 +235,18 @@ private LineMessagingClient lineMessagingClient;
             }
             case "tiga" : {
                 try {
-                    List<ImageCarouselColumn> columns = new ArrayList<>();
+                   /* List<ImageCarouselColumn> columns = new ArrayList<>();
                     columns.add(new ImageCarouselColumn("https://farm1.staticflickr.com/957/28140732828_cb302019b4_m.jpg", null));
                     columns.add(new ImageCarouselColumn("https://pics.me.me/blyat-me-irl-15840371.png", null));
                     columns.add(new ImageCarouselColumn("https://funnypictures4.fjcdn.com/pictures/Cyka+blyat_9dc589_5835010.jpg", null));
                     ImageCarouselTemplate imageCarouselTemplate = new ImageCarouselTemplate(columns);
                     TemplateMessage templateMessage = new TemplateMessage("ImageCarousel alt text", imageCarouselTemplate);
-                    this.reply(replyToken, templateMessage);
+                    this.reply(replyToken, templateMessage);*/
+                    this.reply(replyToken, new ImageMessage(
+                            "https://funnypictures4.fjcdn.com/pictures/Cyka+blyat_9dc589_5835010.jpg",
+                            "https://funnypictures4.fjcdn.com/pictures/Cyka+blyat_9dc589_5835010.jpg"
+                    ));
+                    break;
                 }catch (Exception ex){
                     this.replyText(
                             replyToken,
