5e4d6a8526e09d7072b2890b36151f04f66b461f
Yudhistira Erlandinata <yerlandinata@gmail.com>
2018-05-12 20:32:14 +0700

    disable example bot spring application

--- src/main/java/advprog/example/bot/BotExampleApplication.java
+++ src/main/java/advprog/example/bot/BotExampleApplication.java
@@ -5,7 +5,6 @@ import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class BotExampleApplication {
 
     private static final Logger LOGGER = Logger.getLogger(BotExampleApplication.class.getName());
