c3016f8a0f7f188588e21e837d1c6fc01b2e232c
Yusuf <yfsholeh@gmail.com>
2018-05-12 22:44:45 +0700

    change image url

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -230,7 +230,7 @@ private LineMessagingClient lineMessagingClient;
             }
             case "imagemap":
                 this.reply(replyToken, new ImagemapMessage(
-                        "https://farm1.staticflickr.com/957/28140732828_cb302019b4_z.jpg",
+                        "https://farm1.staticflickr.com/957/28140732828_cb302019b4_z",
                         "This is alt text",
                         new ImagemapBaseSize(1040, 1040),
                         null
