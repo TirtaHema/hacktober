c5ba5246c9bc38059d60bdb3e15d87d7a5c94bb1
Yusuf <yfsholeh@gmail.com>
2018-05-12 23:47:54 +0700

    trying connect to line

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -204,16 +204,6 @@ private LineMessagingClient lineMessagingClient;
                 this.reply(replyToken, templateMessage);
                 break;
             }
-            /*case "gambar_3" : {
-                List<ImageCarouselColumn> columns = new ArrayList<>();
-                columns.add(new ImageCarouselColumn("https://farm1.staticflickr.com/957/28140732828_cb302019b4_m.jpg",null));
-                columns.add(new ImageCarouselColumn("https://pics.me.me/blyat-me-irl-15840371.png",null));
-                columns.add(new ImageCarouselColumn("https://funnypictures4.fjcdn.com/pictures/Cyka+blyat_9dc589_5835010.jpg",null));
-                ImageCarouselTemplate imageCarouselTemplate = new ImageCarouselTemplate(columns);
-                TemplateMessage templateMessage = new TemplateMessage("ImageCarousel alt text", imageCarouselTemplate);
-                this.reply(replyToken, templateMessage);
-                break;
-            }*/
             case "image_carousel": {
                 String imageUrl = createUri("/static/buttons/1040.jpg");
                 ImageCarouselTemplate imageCarouselTemplate = new ImageCarouselTemplate(
@@ -241,6 +231,16 @@ private LineMessagingClient lineMessagingClient;
                         "https://farm1.staticflickr.com/957/28140732828_cb302019b4_m.jpg",
                         "https://farm1.staticflickr.com/957/28140732828_cb302019b4_m.jpg"
                 ));
+                break;
+            }
+            case "gambar_3" : {
+                List<ImageCarouselColumn> columns = new ArrayList<>();
+                columns.add(new ImageCarouselColumn("https://farm1.staticflickr.com/957/28140732828_cb302019b4_m.jpg",null));
+                columns.add(new ImageCarouselColumn("https://pics.me.me/blyat-me-irl-15840371.png",null));
+                columns.add(new ImageCarouselColumn("https://funnypictures4.fjcdn.com/pictures/Cyka+blyat_9dc589_5835010.jpg",null));
+                ImageCarouselTemplate imageCarouselTemplate = new ImageCarouselTemplate(columns);
+                TemplateMessage templateMessage = new TemplateMessage("ImageCarousel alt text", imageCarouselTemplate);
+                this.reply(replyToken, templateMessage);
                 break;
             }
             case "imagemap":
