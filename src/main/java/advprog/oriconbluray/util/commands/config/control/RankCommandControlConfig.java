7d3f75f8a6a072b21068e12edf395f2f5849813b
Samuel Tupa Febrian <samuel.febrian@gmail.com>
2018-05-13 14:17:18 +0700

    implement the rank control config (#6)

--- src/main/java/advprog/oriconbluray/util/commands/config/control/RankCommandControlConfig.java
+++ src/main/java/advprog/oriconbluray/util/commands/config/control/RankCommandControlConfig.java
@@ -1,10 +1,14 @@
 package advprog.oriconbluray.util.commands.config.control;
 
 import advprog.oriconbluray.util.commands.control.RankCommandControl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RankCommandControlConfig {
 
+    @Bean
     RankCommandControl rankCommandControl() {
-        return null;
+        return new RankCommandControl();
     }
 }
