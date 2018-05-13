7abded967e72c73dbc4db8b3dc01429b689107a6
Samuel Tupa Febrian <samuel.febrian@gmail.com>
2018-05-13 14:08:29 +0700

    Create rank control config's stub

---
+++ src/main/java/advprog/oriconbluray/util/commands/config/control/RankCommandControlConfig.java
@@ -0,0 +1,10 @@
+package advprog.oriconbluray.util.commands.config.control;
+
+import advprog.oriconbluray.util.commands.control.RankCommandControl;
+
+public class RankCommandControlConfig {
+
+    RankCommandControl rankCommandControl() {
+        return null;
+    }
+}
