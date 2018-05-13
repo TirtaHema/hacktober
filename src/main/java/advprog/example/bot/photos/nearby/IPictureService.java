1280ece8125cf5ea00244b7f6bfbf11cc8b40ee7
Yusuf <yfsholeh@gmail.com>
2018-05-13 21:02:14 +0700

    changed the controller

--- src/main/java/advprog/example/bot/photos/nearby/IPictureService.java
+++
@@ -1,8 +0,0 @@
-package advprog.example.bot.photos.nearby;
-
-import java.util.List;
-
-public interface IPictureService {
-    public List<Photo> get5Photos(Location location);
-    public String formatTitleForCarouselImages(String title);
-}
