564580dced843b9c9fa7f23a4bb678809cf99324
Norman <normanbintang@hotmail.co.id>
2018-05-14 18:05:09 +0700

    remove unnecessary files

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -1,6 +1,5 @@
 package advprog.example.bot.controller;
 
-import advprog.example.bot.service.FakeJsonHelper;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
@@ -14,32 +13,21 @@ import java.util.logging.Logger;
 public class EchoController {
 
     private static final Logger LOGGER = Logger.getLogger(EchoController.class.getName());
-    private final FakeJsonHelper fakeJsonHelper = new FakeJsonHelper();
 
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-
         LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
                 event.getTimestamp(), event.getMessage()));
         TextMessageContent content = event.getMessage();
         String contentText = content.getText();
 
-        if (contentText.startsWith("/fake_json")) {
-            return new TextMessage(handleFakeJson(contentText));
-
-        } else {
-            String replyText = contentText.replace("/echo", "");
-            return new TextMessage(replyText.substring(1));
-        }
+        String replyText = contentText.replace("/echo", "");
+        return new TextMessage(replyText.substring(1));
     }
 
     @EventMapping
     public void handleDefaultMessage(Event event) {
         LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
                 event.getTimestamp(), event.getSource()));
-    }
-
-    public String handleFakeJson(String contentText) {
-        return fakeJsonHelper.getFakeJsonObject();
     }
 }
