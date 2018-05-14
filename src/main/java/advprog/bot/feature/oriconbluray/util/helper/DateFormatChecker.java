0b08b66c7ded1d5958ab124165ff8e4f67db574f
Samuel Tupa Febrian <samuel.febrian@gmail.com>
2018-05-14 12:49:42 +0700

    Implement date format checker class based on the test (#6)

--- src/main/java/advprog/bot/feature/oriconbluray/util/helper/DateFormatChecker.java
+++ src/main/java/advprog/bot/feature/oriconbluray/util/helper/DateFormatChecker.java
@@ -1,8 +1,30 @@
 package advprog.bot.feature.oriconbluray.util.helper;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class DateFormatChecker {
 
     public String checkAndGetErrorMessage(String date) {
+        SimpleDateFormat formatChecker = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            formatChecker.parse(date);
+        } catch (ParseException e) {
+            return "Hmmmmm, forget about the date format?"
+                    + " No worries~ I can help you~ Here's the format\n\n"
+                    + "YYYY-MM-DD\n\nRemember it well, kay?";
+        }
+
+        formatChecker.setLenient(false);
+
+        try {
+            formatChecker.parse(date);
+        } catch (ParseException e) {
+            return "N-nani?! Is such date even exist?! You would"
+                    + " like to check the calendar... and enter an existing "
+                    + "date...";
+        }
+
         return null;
     }
 }
