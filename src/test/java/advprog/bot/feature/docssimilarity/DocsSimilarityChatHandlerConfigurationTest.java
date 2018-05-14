969c1a4a5f43784777fe3dafd049adf84a243f7b
Norman <normanbintang@hotmail.co.id>
2018-05-14 17:51:11 +0700

    fix import order style

--- src/test/java/advprog/bot/feature/docssimilarity/DocsSimilarityChatHandlerConfigurationTest.java
+++ src/test/java/advprog/bot/feature/docssimilarity/DocsSimilarityChatHandlerConfigurationTest.java
@@ -1,14 +1,14 @@
 package advprog.bot.feature.docssimilarity;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import advprog.bot.BotController;
 import advprog.bot.line.LineChatHandler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DocsSimilarityChatHandlerConfigurationTest {
