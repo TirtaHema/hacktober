41e2ab298d1242d68aff59932bc288f5dade3148
Mohammad Ammar Ramadhan <mohammad.ammar61@ui.ac.id>
2018-05-15 00:51:57 +0700

    Edit ApiConfig Imagga

--- src/main/java/advprog/imagga/config/ApiConfig.java
+++ src/main/java/advprog/imagga/config/ApiConfig.java
@@ -8,10 +8,9 @@ import java.util.Base64;
 
 public class ApiConfig {
 
-    private static String key = "API_KEY";
-    private static String secret = "API_SECRET";
+    private static String key = "acc_c79690c6c890ef8";
+    private static String secret = "bad73ff16021bc16bcf944d0b40e9a3c";
     public final static String URL = "https://api.imagga.com/v1";
-    private static HttpURLConnection connection;
 
     public static String getAuth() {
         String credentialsToEncode = key + ":" + secret;
