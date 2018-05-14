d31fad8557e80ad67121e2befae143ff4174a717
Yudhistira Erlandinata <yerlandinata@gmail.com>
2018-05-14 14:56:42 +0700

    implement youtube info chat handler

--- src/main/java/advprog/bot/feature/yerlandinata/youtubeinfo/YoutubeInfoChatHandler.java
+++ src/main/java/advprog/bot/feature/yerlandinata/youtubeinfo/YoutubeInfoChatHandler.java
@@ -1,6 +1,8 @@
 package advprog.bot.feature.yerlandinata.youtubeinfo;
 
 import advprog.bot.feature.yerlandinata.youtubeinfo.fetcher.YoutubeInfoFetcher;
+import advprog.bot.feature.yerlandinata.youtubeinfo.fetcher.YoutubeVideoNotFoundException;
+import advprog.bot.feature.yerlandinata.youtubeinfo.parser.AbstractYoutubeVideoIdParser;
 import advprog.bot.line.AbstractLineChatHandlerDecorator;
 import advprog.bot.line.LineChatHandler;
 
@@ -11,30 +13,68 @@ import com.linecorp.bot.model.event.message.LocationMessageContent;
 import com.linecorp.bot.model.event.message.StickerMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
+
+import org.json.JSONException;
 
 public class YoutubeInfoChatHandler extends AbstractLineChatHandlerDecorator {
 
     private final YoutubeInfoFetcher youtubeInfoFetcher;
+    private final AbstractYoutubeVideoIdParser youtubeVideoIdParser;
+    private static final String COMMAND_WORD = "/youtube";
+    private static final Logger LOGGER = Logger.getLogger(YoutubeInfoChatHandler.class.getName());
+    static final String MESSAGE_INVALID_URL = "URL Video Youtube tidak sah :(";
 
-    public YoutubeInfoChatHandler(LineChatHandler decoratedHandler, YoutubeInfoFetcher youtubeInfoFetcher) {
+    public YoutubeInfoChatHandler(
+            LineChatHandler decoratedHandler,
+            YoutubeInfoFetcher youtubeInfoFetcher,
+            AbstractYoutubeVideoIdParser youtubeVideoIdParser) {
         this.decoratedLineChatHandler = decoratedHandler;
         this.youtubeInfoFetcher = youtubeInfoFetcher;
+        this.youtubeVideoIdParser = youtubeVideoIdParser;
     }
 
     @Override
     protected boolean canHandleTextMessage(MessageEvent<TextMessageContent> event) {
-        return false;
-    }
-
-    private String getVideoIdFromUrl(String videoUrl) {
-        return null;
+        return event.getMessage().getText().split(" ")[0].equals(COMMAND_WORD);
     }
 
     @Override
     protected List<Message> handleTextMessage(MessageEvent<TextMessageContent> event) {
-        return null;
+        LOGGER.info("Trying to handle message: " + event.getMessage().getText());
+        String videoUrl = event.getMessage().getText()
+                                            .split(" ")[1]
+                                            .trim();
+        String videoId;
+        try {
+            videoId = youtubeVideoIdParser.parseYoutubeVideoId(videoUrl);
+        } catch (Exception e) {
+            return Collections.singletonList(
+                    new TextMessage(MESSAGE_INVALID_URL)
+            );
+        }
+        YoutubeVideo youtubeVideo;
+        try {
+            youtubeVideo = youtubeInfoFetcher.fetchData(videoId);
+        } catch (IOException | JSONException | YoutubeVideoNotFoundException e){
+            String reply = "Video dengan ID " + videoId + " tidak dapat kami akses :(";
+            LOGGER.info("Fail, reply:\n" + reply);
+            return Collections.singletonList(new TextMessage(reply));
+        }
+        String reply = "Informasi video"
+                + "\nJudul: " + youtubeVideo.getTitle()
+                + "\nChannel: " + youtubeVideo.getChannelName()
+                + "\nJumlah ditonton: " +  youtubeVideo.getViewCount() + " kali"
+                + "\nLikes: " + youtubeVideo.getLikeCount()
+                + "\nDislikes: " + youtubeVideo.getDislikeCount()
+                + "\nURL: " + videoUrl;
+        LOGGER.info("Success, reply:\n" + reply);
+        return Collections.singletonList(new TextMessage(reply));
     }
 
     @Override
--- src/test/java/advprog/bot/feature/yerlandinata/youtubeinfo/YoutubeInfoChatHandlerTest.java
+++ src/test/java/advprog/bot/feature/yerlandinata/youtubeinfo/YoutubeInfoChatHandlerTest.java
@@ -1,13 +1,18 @@
 package advprog.bot.feature.yerlandinata.youtubeinfo;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import advprog.bot.ChatHandlerTestUtil;
 import advprog.bot.feature.yerlandinata.youtubeinfo.fetcher.YoutubeInfoFetcher;
+import advprog.bot.feature.yerlandinata.youtubeinfo.fetcher.YoutubeVideoNotFoundException;
+import advprog.bot.feature.yerlandinata.youtubeinfo.parser.AbstractYoutubeVideoIdParser;
+import advprog.bot.feature.yerlandinata.youtubeinfo.parser.InvalidYoutubeVideoUrl;
 import advprog.bot.line.BaseChatHandler;
 
 import com.linecorp.bot.model.event.MessageEvent;
@@ -33,12 +38,15 @@ public class YoutubeInfoChatHandlerTest {
     @Mock
     YoutubeInfoFetcher youtubeInfoFetcher;
 
+    @Mock
+    AbstractYoutubeVideoIdParser youtubeVideoIdParser;
+
     YoutubeInfoChatHandler youtubeInfoChatHandler;
 
     @Before
     public void setUp() {
         youtubeInfoChatHandler = new YoutubeInfoChatHandler(
-                new BaseChatHandler(), youtubeInfoFetcher
+                new BaseChatHandler(), youtubeInfoFetcher, youtubeVideoIdParser
         );
     }
 
@@ -64,6 +72,8 @@ public class YoutubeInfoChatHandlerTest {
         MessageEvent<TextMessageContent> me = ChatHandlerTestUtil.fakeMessageEvent("re", text);
         when(youtubeInfoFetcher.fetchData(any(String.class)))
                 .thenReturn(expectedVideo);
+        when(youtubeVideoIdParser.parseYoutubeVideoId(any(String.class)))
+                .thenReturn(videoId);
 
         List<Message> expectedReplies = Collections.singletonList(new TextMessage(expectedOutput));
         List<Message> actualReplies = youtubeInfoChatHandler.handleTextMessageEvent(
@@ -71,7 +81,76 @@ public class YoutubeInfoChatHandlerTest {
         );
 
         verify(youtubeInfoFetcher).fetchData(eq(videoId));
+        verify(youtubeVideoIdParser).parseYoutubeVideoId(eq(validUrl));
         assertEquals(expectedReplies, actualReplies);
+    }
+
+    @Test
+    public void testHandleCorrectCommandInvalidUrl() throws IOException, JSONException {
+        String videoId = "videoId";
+        String invalidUrl = "https://yutube.com/watch?v=" + videoId;
+        String text = "/youtube " + invalidUrl;
+        when(youtubeVideoIdParser.parseYoutubeVideoId(any(String.class)))
+                .thenThrow(new InvalidYoutubeVideoUrl("Invalid URL"));
+
+        MessageEvent<TextMessageContent> me = ChatHandlerTestUtil.fakeMessageEvent("re", text);
+
+        List<Message> expectedReplies = Collections.singletonList(
+                new TextMessage(YoutubeInfoChatHandler.MESSAGE_INVALID_URL)
+        );
+        List<Message> actualReplies = youtubeInfoChatHandler.handleTextMessageEvent(
+                me, new LinkedList<>()
+        );
+
+        verify(youtubeVideoIdParser).parseYoutubeVideoId(eq(invalidUrl));
+        verify(youtubeInfoFetcher, never()).fetchData(any(String.class));
+
+        assertEquals(expectedReplies, actualReplies);
+    }
+
+    @Test
+    public void testHandleVideoNotFound() throws IOException, JSONException {
+        String videoId = "videoId";
+        String validUrl = "https://youtube.com/watch?v=" + videoId;
+        String text = "/youtube " + validUrl;
+        String expectedOutput = "Video dengan ID " + videoId + " tidak dapat kami akses :(";
+        MessageEvent<TextMessageContent> me = ChatHandlerTestUtil.fakeMessageEvent("re", text);
+        when(youtubeInfoFetcher.fetchData(any(String.class)))
+                .thenThrow(new YoutubeVideoNotFoundException(""));
+        when(youtubeVideoIdParser.parseYoutubeVideoId(any(String.class)))
+                .thenReturn(videoId);
+
+        List<Message> expectedReplies = Collections.singletonList(new TextMessage(expectedOutput));
+        List<Message> actualReplies = youtubeInfoChatHandler.handleTextMessageEvent(
+                me, new LinkedList<>()
+        );
+
+        verify(youtubeInfoFetcher).fetchData(eq(videoId));
+        verify(youtubeVideoIdParser).parseYoutubeVideoId(eq(validUrl));
+        assertEquals(expectedReplies, actualReplies);
+    }
+
+    @Test
+    public void testNotHandleIncorrectCommand() throws IOException, JSONException {
+        String invalidCmd = "/google https://google.com/search?q=videoyutub";
+        List<Message> previousMessages = Collections.singletonList(
+                new TextMessage("previous messages")
+        );
+        MessageEvent<TextMessageContent> me = ChatHandlerTestUtil.fakeMessageEvent("re", invalidCmd);
+        assertEquals(
+                previousMessages,
+                youtubeInfoChatHandler.handleTextMessageEvent(me, previousMessages)
+        );
+        verify(youtubeInfoFetcher, never()).fetchData(any(String.class));
+        verify(youtubeVideoIdParser, never()).parseYoutubeVideoId(any(String.class));
+    }
+
+    @Test
+    public void testIgnoreNonTextMessageEvent() {
+        assertFalse(youtubeInfoChatHandler.canHandleAudioMessage(null));
+        assertFalse(youtubeInfoChatHandler.canHandleImageMessage(null));
+        assertFalse(youtubeInfoChatHandler.canHandleStickerMessage(null));
+        assertFalse(youtubeInfoChatHandler.canHandleLocationMessage(null));
     }
 
 }
