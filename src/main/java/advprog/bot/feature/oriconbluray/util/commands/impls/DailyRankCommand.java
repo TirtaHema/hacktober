6e623618a9c12ebaaa277bdc876f21f193695ca0
Samuel Tupa Febrian <samuel.febrian@gmail.com>
2018-05-14 12:53:34 +0700

    Implement date format checker on the weekly and daily command (extract method) (#6)

--- src/main/java/advprog/bot/feature/oriconbluray/util/commands/impls/DailyRankCommand.java
+++ src/main/java/advprog/bot/feature/oriconbluray/util/commands/impls/DailyRankCommand.java
@@ -1,12 +1,11 @@
 package advprog.bot.feature.oriconbluray.util.commands.impls;
 
 import advprog.bot.feature.oriconbluray.util.commands.interfaces.RankCommand;
+import advprog.bot.feature.oriconbluray.util.helper.DateFormatChecker;
 import advprog.bot.feature.oriconbluray.util.service.rankscrapper.RankScrapper;
 import com.linecorp.bot.model.message.TextMessage;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import org.springframework.stereotype.Component;
 
@@ -14,12 +13,13 @@ import org.springframework.stereotype.Component;
 public class DailyRankCommand implements RankCommand {
 
     private RankScrapper rankScrapper = new RankScrapper();
+    private DateFormatChecker dateChecker = new DateFormatChecker();
 
     private final String dailyUrl = "https://www.oricon.co.jp/rank/bd/d/";
 
     @Override
     public TextMessage execute(String date) throws IOException {
-        String errorMsg = dateCheck(date);
+        String errorMsg = dateChecker.checkAndGetErrorMessage(date);
         if (errorMsg != null) {
             return new TextMessage(errorMsg);
         }
@@ -31,28 +31,5 @@ public class DailyRankCommand implements RankCommand {
                 ? "It seems there's no rank charts on that date"
                 : rankList;
         return new TextMessage(rankList);
-    }
-
-    private String dateCheck(String date) {
-        SimpleDateFormat formatChecker = new SimpleDateFormat("yyyy-MM-dd");
-        try {
-            formatChecker.parse(date);
-        } catch (ParseException e) {
-            return "Hmmmmm, forget about the date format?"
-                    + " No worries~ I can help you~ Here's the format\n\n"
-                    + "YYYY-MM-DD\n\nRemember it well, kay?";
-        }
-
-        formatChecker.setLenient(false);
-
-        try {
-            formatChecker.parse(date);
-        } catch (ParseException e) {
-            return "N-nani?! Is such date even exist?! You would"
-                    + " like to check the calendar... and enter an existing "
-                    + "date...";
-        }
-
-        return null;
     }
 }
