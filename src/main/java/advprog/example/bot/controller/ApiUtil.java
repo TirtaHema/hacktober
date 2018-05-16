538bb78ba09c2a8eafc0a3054de87273186aaba1
luqi <khoiruluqi@gmail.com>
2018-05-16 12:45:22 +0700

    Connecting line bot with Microsoft Azzure API

--- src/main/java/advprog/example/bot/controller/ApiUtil.java
+++ src/main/java/advprog/example/bot/controller/ApiUtil.java
@@ -1,19 +1,19 @@
-package advprog.handwrittenNotesExtractedIntoText.bot;
+package advprog.example.bot.controller;
 
 import java.net.URI;
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.EntityBuilder;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.apache.http.Header;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class APIUtil {
+public class ApiUtil {
     // **********************************************
     // *** Update or verify the following values. ***
     // **********************************************
@@ -23,19 +23,22 @@ public class APIUtil {
 
     // Replace or verify the region.
     //
-    // You must use the same region in your REST API call as you used to obtain your subscription keys.
+    // You must use the same region in your REST API call as you used to obtain your
+    // subscription keys.
     // For example, if you obtained your subscription keys from the westus region, replace
     // "westcentralus" in the URI below with "westus".
     //
-    // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+    // NOTE: Free trial subscription keys are generated in the westcentralus region,
+    // so if you are using
     // a free trial subscription key, you should not need to change this region.
     //
     // Also, for printed text, set "handwriting" to false.
-    public static final String uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/recognizeText?handwriting=true";
+    public static final String uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision"
+            + "/v1.0/recognizeText?handwriting=true";
 
     public static String imageToText(byte[] binaryBytes) {
-        HttpClient textClient = new DefaultHttpClient();
-        HttpClient resultClient = new DefaultHttpClient();
+        HttpClient textClient = HttpClients.createDefault();
+        HttpClient resultClient = HttpClients.createDefault();
 
         try {
             // This operation requrires two REST API calls. One to submit the image for processing,
@@ -48,10 +51,10 @@ public class APIUtil {
             // Request headers. Another valid content type is "application/octet-stream".
             textRequest.setHeader("Content-Type", "application/octet-stream");
             textRequest.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-            textRequest.setHeader("Authorization", "Bearer WWGPTyNm/uJLr7lg+" +
-                    "AgXSx3SicYp0FjWHOoVJxseKo29WOM9fGwCFtHoRLzaaNolh985FYYIMmiUKPY423AFw" +
-                    "mEu1E0Cy+KgJLFhlj+B47B4slICH0cbHmNxGr4NZLLn9EXE9uzVstsyFUn12Y/n6gdB0" +
-                    "4t89/1O/w1cDnyilFU=");
+            textRequest.setHeader("Authorization", "Bearer WWGPTyNm/uJLr7lg+"
+                    + "AgXSx3SicYp0FjWHOoVJxseKo29WOM9fGwCFtHoRLzaaNolh985FYYIMmiUKPY423AFw"
+                    + "mEu1E0Cy+KgJLFhlj+B47B4slICH0cbHmNxGr4NZLLn9EXE9uzVstsyFUn12Y/n6gdB0"
+                    + "4t89/1O/w1cDnyilFU=");
 
             // Request body.
             HttpEntity requestEntity = EntityBuilder.create().setBinary(binaryBytes).build();
@@ -71,21 +74,25 @@ public class APIUtil {
 
             String operationLocation = null;
 
-            // The 'Operation-Location' in the response contains the URI to retrieve the recognized text.
+            // The 'Operation-Location' in the response contains the URI to retrieve the
+            // recognized text.
             Header[] responseHeaders = textResponse.getAllHeaders();
             for (Header header : responseHeaders) {
                 if (header.getName().equals("Operation-Location")) {
-                    // This string is the URI where you can get the text recognition operation result.
+                    // This string is the URI where you can get the text recognition
+                    // operation result.
                     operationLocation = header.getValue();
                     break;
                 }
             }
 
-            // NOTE: The response may not be immediately available. Handwriting recognition is an
+            // NOTE: The response may not be immediately available. Handwriting recognition
+            // is an
             // async operation that can take a variable amount of time depending on the length
             // of the text you want to recognize. You may need to wait or retry this operation.
 
-            System.out.println("\nHandwritten text submitted. Waiting 10 seconds to retrieve the recognized text.\n");
+            System.out.println("\nHandwritten text submitted. Waiting 10 seconds to"
+                    + "retrieve the recognized text.\n");
             Thread.sleep(10000);
 
             // Execute the second REST API call and get the response.
--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -1,18 +1,32 @@
 package advprog.example.bot.controller;
 
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.client.MessageContentResponse;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.ImageMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
+import org.apache.commons.io.IOUtils;
 
 @LineMessageHandler
 public class EchoController {
 
+    private LineMessagingClient lineMessagingClient;
+
     private static final Logger LOGGER = Logger.getLogger(EchoController.class.getName());
+
+    static {
+        System.setProperty("line.bot.channelSecret", "SECRET");
+        System.setProperty("line.bot.channelToken", "TOKEN");
+    }
 
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
@@ -23,7 +37,29 @@ public class EchoController {
 
         String replyText = contentText.replace("/echo", "");
         return new TextMessage(replyText.substring(1));
-        event.ge
+    }
+
+    @EventMapping
+    public TextMessage handleImageMessageEvent(MessageEvent<ImageMessageContent> event)
+            throws IOException {
+        final LineMessagingClient client = LineMessagingClient
+                .builder(System.getProperty("line.bot.channelToken"))
+                .build();
+
+        final MessageContentResponse messageContentResponse;
+        try {
+            messageContentResponse = client.getMessageContent(event.getMessage().getId()).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return new TextMessage("Error!");
+        }
+
+        InputStream contentStream = messageContentResponse.getStream();
+        byte[] contentBytes = IOUtils.toByteArray(contentStream);
+
+        String result = ApiUtil.imageToText(contentBytes);
+
+        return new TextMessage(result);
     }
 
     @EventMapping
@@ -31,4 +67,5 @@ public class EchoController {
         LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
                 event.getTimestamp(), event.getSource()));
     }
+
 }
