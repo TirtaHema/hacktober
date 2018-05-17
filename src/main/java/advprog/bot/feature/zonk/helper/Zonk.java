fb54adf4f76bfccd5f069a2efffaf3b6d3994afa
Tirta Hema Jaya Hidayat <tirta.hema@ui.ac.id>
2018-05-17 23:01:56 +0700

    making unit test

--- src/main/java/advprog/bot/feature/zonk/helper/Zonk.java
+++ src/main/java/advprog/bot/feature/zonk/helper/Zonk.java
@@ -1,17 +1,15 @@
 package advprog.bot.feature.zonk.helper;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Iterator;
+
+
 
 public class Zonk {
-    public static String terima(String input){
+
+    public Zonk(String input){
+        String masukan = input;
 
     }
+
+
 }
--- src/test/java/advprog/bot/feature/helper/ZonkTest.java
+++ src/test/java/advprog/bot/feature/helper/ZonkTest.java
@@ -1,4 +1,45 @@
 package advprog.bot.feature.helper;
 
+import advprog.bot.feature.zonk.helper.Zonk;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class ZonkTest {
+    private Zonk zonk;
+
+    @Before
+    public void setUp() {
+        zonk = new Zonk("haha");
+    }
+    @Test
+    public void testMethodJawab() {
+        assertEquals("benar",zonk.jawab(1,"mc donald phone number ?"));
+    }
+    @Test
+    public void testMethodBuatSoal() {
+        String soal = zonk.soal("what is the dog name");
+        assertEquals(soal,zonk.buatSoal({"what is the dog name","heli","helo","hi","hoo","heli"}));
+    }
+    @Test
+    public void testMethodSoal() {
+        String soal = zonk.soal("mc donald phone number ?");
+        assertEquals(soal,zonk.soal("mc donald phone number ?"));
+    }
+    @Test
+    public void testMethodGantiSoal() {
+        assertEquals("indomie\napple\nnutrisari\ntoyota\napple",zonk.Gantisoal("mc donald phone number ?","apple"));
+    }
+
 }
