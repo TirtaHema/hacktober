cdc5fbdbc7abbb39d1d46608118df7a34808dae2
Tirta Hema Jaya Hidayat <tirta.hema@ui.ac.id>
2018-05-18 00:04:20 +0700

    making stubs

--- src/main/java/advprog/bot/feature/zonk/helper/Zonk.java
+++ src/main/java/advprog/bot/feature/zonk/helper/Zonk.java
@@ -1,10 +1,29 @@
 package advprog.bot.feature.zonk.helper;
 
 
-
-
-
 public class Zonk {
+
+    public static void buatSoal (String[] soal){
+
+    }
+
+    public static String gantiSoal(String soal,String jawaban){
+
+        return "jawaban terganti";
+    }
+
+    public static String soal(String soal){
+
+
+        return "ini soalnya....";
+    }
+
+    public static String jawab(int input, String soal){
+
+
+        return "jawaban anda benar !";
+
+    }
 
     public Zonk(String input){
         String masukan = input;
