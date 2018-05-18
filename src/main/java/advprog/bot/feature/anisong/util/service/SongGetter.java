f122706e7fcf90010dd2cd64f1cb9cc066e13a27
Ryan Naufal <ryan.naufal@ui.ac.id>
2018-05-18 19:07:30 +0700

    checkStyleMain once again because of lexicograpic import

--- src/main/java/advprog/bot/feature/anisong/util/service/SongGetter.java
+++ src/main/java/advprog/bot/feature/anisong/util/service/SongGetter.java
@@ -1,9 +1,9 @@
 package advprog.bot.feature.anisong.util.service;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.IOException;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
--- src/main/java/advprog/bot/feature/anisong/util/service/SongRecognizer.java
+++ src/main/java/advprog/bot/feature/anisong/util/service/SongRecognizer.java
@@ -1,9 +1,9 @@
 package advprog.bot.feature.anisong.util.service;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.IOException;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
@@ -22,7 +22,8 @@ public class SongRecognizer {
         url += song;
         InputStream is = new URL(url).openStream();
         try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(is, Charset.forName("UTF-8")));
             String json = reader(rd);
             JSONObject jsonObject = new JSONObject(json);
             JSONObject jsonobj = ((JSONArray) jsonObject.get("results")).getJSONObject(0);
