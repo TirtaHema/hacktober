ad736d0788d729c93504b8dd5e2cdb08d102b7fe
regensa18 <regensa.cahyanto18@gmail.com>
2018-05-18 10:43:18 +0700

    added test

--- src/test/java/advprog/bot/feature/itunes/iTunesChatHandlerConfigurationTest.java
+++ src/test/java/advprog/bot/feature/itunes/iTunesChatHandlerConfigurationTest.java
@@ -1,4 +1,27 @@
 package advprog.bot.feature.itunes;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import advprog.bot.BotController;
+import advprog.bot.feature.itunes.iTunesChatHandler;
+import advprog.bot.feature.itunes.iTunesChatHandlerConfiguration;
+import advprog.bot.line.LineChatHandler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class iTunesChatHandlerConfigurationTest {
+    @Test
+    public void testConstruction() {
+        BotController controller = mock(BotController.class);
+        LineChatHandler decoratedHandler = mock(LineChatHandler.class);
+        when(controller.getLineChatHandler()).thenReturn(decoratedHandler);
+        iTunesChatHandlerConfiguration configuration = new iTunesChatHandlerConfiguration();
+        iTunesChatHandler itunesChatHandler = configuration.billboardChatHandler(controller);
+        assertEquals(decoratedHandler, itunesChatHandler.getDecoratedLineChatHandler());
+    }
 }
