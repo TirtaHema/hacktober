195d87897c40f18f4060b774a0f97ac1d557c29d
Yudhistira Erlandinata <yerlandinata@gmail.com>
2018-05-19 00:38:41 +0700

    implement showing first 10 surah as carousel

--- src/main/java/advprog/bot/feature/yerlandinata/quran/privatechat/interactive/InteractivePrivateQuranChatHandler.java
+++ src/main/java/advprog/bot/feature/yerlandinata/quran/privatechat/interactive/InteractivePrivateQuranChatHandler.java
@@ -1,7 +1,13 @@
 package advprog.bot.feature.yerlandinata.quran.privatechat.interactive;
 
+import advprog.bot.feature.yerlandinata.quran.SurahQuran;
+import advprog.bot.feature.yerlandinata.quran.fetcher.AyatQuranFetcher;
+import advprog.bot.feature.yerlandinata.quran.fetcher.SurahQuranFetcher;
+import advprog.bot.feature.yerlandinata.quran.privatechat.interactive.service.InteractiveAyatFetcherService;
 import advprog.bot.line.AbstractLineChatHandlerDecorator;
+import advprog.bot.line.LineChatHandler;
 
+import com.linecorp.bot.model.action.MessageAction;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.AudioMessageContent;
 import com.linecorp.bot.model.event.message.ImageMessageContent;
@@ -9,14 +15,52 @@ import com.linecorp.bot.model.event.message.LocationMessageContent;
 import com.linecorp.bot.model.event.message.StickerMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TemplateMessage;
+import com.linecorp.bot.model.message.template.CarouselColumn;
+import com.linecorp.bot.model.message.template.CarouselTemplate;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import org.json.JSONException;
 
 public class InteractivePrivateQuranChatHandler extends AbstractLineChatHandlerDecorator {
+
+    static final String QURAN_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/"
+            + "thumb/7/7b/Opened_Qur%27an.jpg/220px-Opened_Qur%27an.jpg";
+
+    private final SurahQuranFetcher surahQuranFetcher;
+    private final InteractiveAyatFetcherService interactiveAyatFetcherService;
+
+    private static final Pattern QS_PATTERN = Pattern.compile("/qs");
+    private static final Pattern QSI_NEXT_PATTERN = Pattern.compile("/qsi \\d+:\\d+");
+    private static final Pattern QSI_CHOOSE_PATTERN = Pattern.compile("/qsi \\d+");
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("\\d+");
+    private static final Logger LOGGER = Logger.getLogger(
+            InteractivePrivateQuranChatHandler.class.getName()
+    );
+
+    public InteractivePrivateQuranChatHandler(
+            LineChatHandler decoratedHandler,
+            SurahQuranFetcher surahQuranFetcher,
+            InteractiveAyatFetcherService interactiveAyatFetcherService
+    ) {
+        this.decoratedLineChatHandler = decoratedHandler;
+        this.surahQuranFetcher = surahQuranFetcher;
+        this.interactiveAyatFetcherService = interactiveAyatFetcherService;
+        LOGGER.info("Interactive Quran Private Chat Handler set!");
+    }
+
     @Override
     protected boolean canHandleTextMessage(MessageEvent<TextMessageContent> event) {
-        return false;
+        return QS_PATTERN.matcher(event.getMessage().getText()).matches()
+                || QSI_NEXT_PATTERN.matcher(event.getMessage().getText()).matches()
+                || QSI_CHOOSE_PATTERN.matcher(event.getMessage().getText()).matches()
+                || NUMBER_PATTERN.matcher(event.getMessage().getText()).matches();
     }
 
     /*
@@ -27,7 +71,38 @@ public class InteractivePrivateQuranChatHandler extends AbstractLineChatHandlerD
     */
     @Override
     protected List<Message> handleTextMessage(MessageEvent<TextMessageContent> event) {
+        if (QS_PATTERN.matcher(event.getMessage().getText()).matches()) {
+            try {
+                return showSurah(1, 10);
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+        }
         return Collections.emptyList();
+    }
+
+    private List<Message> showSurah(int begin, int end) throws IOException, JSONException {
+        return Collections.singletonList(
+                new TemplateMessage(
+                        "Quran",
+                        new CarouselTemplate(
+                                surahQuranFetcher.fetchSurahQuran()
+                                        .subList(begin - 1, end)
+                                        .stream()
+                                        .map(s -> new CarouselColumn(
+                                                QURAN_IMAGE,
+                                                s.getEnglishName(),
+                                                s.getArabName(),
+                                                Collections.singletonList(
+                                                        new MessageAction(
+                                                                "Pilih",
+                                                                "/qsi " + s.getSurahNumber()
+                                                        )
+                                                )
+                                        ))
+                                        .collect(Collectors.toList())
+                        ))
+        );
     }
 
     @Override
--- src/test/java/advprog/bot/feature/yerlandinata/quran/privatechat/interactive/InteractivePrivateQuranChatHandlerTest.java
+++ src/test/java/advprog/bot/feature/yerlandinata/quran/privatechat/interactive/InteractivePrivateQuranChatHandlerTest.java
@@ -1,9 +1,100 @@
 package advprog.bot.feature.yerlandinata.quran.privatechat.interactive;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import advprog.bot.ChatHandlerTestUtil;
+import advprog.bot.feature.yerlandinata.quran.SurahQuran;
+import advprog.bot.feature.yerlandinata.quran.fetcher.SurahQuranFetcher;
+import advprog.bot.feature.yerlandinata.quran.privatechat.interactive.service.InteractiveAyatFetcherService;
+import advprog.bot.line.BaseChatHandler;
+
+import com.linecorp.bot.model.action.MessageAction;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.source.UserSource;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TemplateMessage;
+import com.linecorp.bot.model.message.template.CarouselColumn;
+import com.linecorp.bot.model.message.template.CarouselTemplate;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class InteractivePrivateQuranChatHandlerTest {
+
+    @Mock
+    SurahQuranFetcher surahQuranFetcher;
+
+    @Mock
+    InteractiveAyatFetcherService interactiveAyatFetcherService;
+
+    InteractivePrivateQuranChatHandler quranChatHandler;
+
+    @Before
+    public void setUp() {
+        quranChatHandler = new InteractivePrivateQuranChatHandler(
+                new BaseChatHandler(), surahQuranFetcher, interactiveAyatFetcherService
+        );
+    }
+
+    @Test
+    public void testShowCarousel10Surah() throws IOException, JSONException {
+        List<SurahQuran> expectedSurah = IntStream.range(0, 15)
+                                            .mapToObj(i -> new SurahQuran(i, i, "" + i, "title" + i))
+                                            .collect(Collectors.toList());
+
+        when(surahQuranFetcher.fetchSurahQuran())
+                .thenReturn(expectedSurah);
+        CarouselTemplate expectedCarousel = new CarouselTemplate(
+                expectedSurah
+                        .stream()
+                        .limit(10)
+                        .map(s -> new CarouselColumn(
+                                InteractivePrivateQuranChatHandler.QURAN_IMAGE,
+                                s.getEnglishName(),
+                                s.getArabName(),
+                                Collections.singletonList(
+                                        new MessageAction(
+                                                "Pilih", "/qsi " + s.getSurahNumber()
+                                        )
+                                )
+                        )).collect(Collectors.toList())
+        );
+        MessageEvent<TextMessageContent> me = ChatHandlerTestUtil.fakeMessageEvent(
+                "re", "/qs", new UserSource("x")
+        );
+        List<Message> expectedMessage = Collections.singletonList(
+            new TemplateMessage("Quran", expectedCarousel)
+        );
+        List<Message> actualMessage = quranChatHandler.handleTextMessageEvent(
+                me, new LinkedList<>()
+        );
+        assertEquals(expectedMessage, actualMessage);
+        verify(surahQuranFetcher).fetchSurahQuran();
+    }
+
+    @Test
+    public void testIgnoreNonTextMessageEvent() {
+        assertFalse(quranChatHandler.canHandleAudioMessage(null));
+        assertFalse(quranChatHandler.canHandleImageMessage(null));
+        assertFalse(quranChatHandler.canHandleStickerMessage(null));
+        assertFalse(quranChatHandler.canHandleLocationMessage(null));
+    }
 
 }
