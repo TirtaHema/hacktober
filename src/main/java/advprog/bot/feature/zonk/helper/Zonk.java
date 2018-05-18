c2c4d713ff8cd1700df68d530f3dcdba2366d3a3
Tirta Hema Jaya Hidayat <tirta.hema@ui.ac.id>
2018-05-18 17:17:38 +0700

    making partial implementation and make sure ZonkTest have 100% coverage

--- src/main/java/advprog/bot/feature/zonk/helper/Zonk.java
+++ src/main/java/advprog/bot/feature/zonk/helper/Zonk.java
@@ -1,29 +1,148 @@
 package advprog.bot.feature.zonk.helper;
 
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import java.util.ArrayList;
+
+
+
+
+
 public class Zonk {
 
-    public static void buatSoal (String[] soal){
+    public static void buatSoal(String[] soal) {
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject obj = (JSONObject) parser.parse(new FileReader("C:\\Users\\tirta\\Desktop\\AP\\B3\\Question.json"));
+            JSONArray arr = new JSONArray();
+            String question =  soal[0];
+            for (int i =1; i<soal.length; i++){
+                arr.add(soal[i]);
+            }
+            obj.put(question,arr);
+            FileWriter fw = new FileWriter("C:\\Users\\tirta\\Desktop\\AP\\B3\\Question.json");
+            fw.write(obj.toString());
+            fw.close();
+
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    public static String option (String soal){
+        String option = "";
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("C:\\Users\\tirta\\Desktop\\AP\\B3\\Question.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray arr = (JSONArray) jsonObject.get(soal);
+            for (int i=0;i<arr.size();i++){
+                option += arr.get(i).toString();
+            }
+            return option;
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return option;
 
     }
 
-    public static String gantiSoal(String soal,String jawaban){
 
-        return "jawaban terganti";
+    public static void gantiSoal(String soal,String jawaban){
+        JSONParser parser = new JSONParser();
+
+        try {
+
+            Object obj = parser.parse(new FileReader("C:\\Users\\tirta\\Desktop\\AP\\B3\\Question.json"));
+
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray arr = (JSONArray) jsonObject.get(soal);
+            ArrayList<String> temp = new ArrayList<>();
+            String ans = arr.get(4).toString();
+            for (int i=0;i<arr.size();i++){
+                temp.add(arr.get(i).toString());
+
+            }
+            arr.removeAll(temp);
+            for (int i=0;i<temp.size();i++){
+                if (temp.get(i).equals(ans)){
+                    temp.set(i,jawaban);
+                }
+                arr.add(temp.get(i));
+            }
+            jsonObject.remove(soal);
+            jsonObject.put(soal,arr);
+            FileWriter fw = new FileWriter("C:\\Users\\tirta\\Desktop\\AP\\B3\\Question.json");
+            fw.write(jsonObject.toString());
+            fw.close();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
-    public static String soal(String soal){
-
-
-        return "ini soalnya....";
-    }
 
     public static String jawab(int input, String soal){
+        JSONParser parser = new JSONParser();
 
+        try {
 
-        return "jawaban anda benar !";
+            Object obj = parser.parse(new FileReader("C:\\Users\\tirta\\Desktop\\AP\\B3\\Question.json"));
+
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray arr = (JSONArray) jsonObject.get(soal);
+            String jawaban = arr.get(4).toString();
+            String masukan = arr.get(input).toString();
+            if (jawaban.equals(masukan)){
+                return "benar";
+            }
+            else {
+                return "salah";
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return "lol";
 
     }
+    public static void main (String[] args){
+        String[] soal = {"who are","tony stark","budi","i dont know","hulk","tony stark"};
+        gantiSoal("who are","boy");
+        System.out.println("dor");
+
+    }
+
 
     public Zonk(String input){
         String masukan = input;
--- src/test/java/advprog/bot/feature/helper/ZonkTest.java
+++ src/test/java/advprog/bot/feature/helper/ZonkTest.java
@@ -1,15 +1,7 @@
 package advprog.bot.feature.helper;
 
 import advprog.bot.feature.zonk.helper.Zonk;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Iterator;
 
 import static org.junit.Assert.assertEquals;
 
@@ -28,18 +20,26 @@ public class ZonkTest {
         assertEquals("benar",zonk.jawab(1,"mc donald phone number ?"));
     }
     @Test
-    public void testMethodBuatSoal() {
-        String soal = zonk.soal("what is the dog name");
-        assertEquals(soal,zonk.buatSoal({"what is the dog name","heli","helo","hi","hoo","heli"}));
+    public void testMethodJawabSalah() {
+        assertEquals("salah",zonk.jawab(0,"mc donald phone number ?"));
     }
     @Test
-    public void testMethodSoal() {
-        String soal = zonk.soal("mc donald phone number ?");
-        assertEquals(soal,zonk.soal("mc donald phone number ?"));
+    public void testMethodBuatSoal() {
+        String[] question = {"what is the dog name?","helo","hela","heli","helu","heli"};
+        zonk.buatSoal(question);
+        String jawaban = "helohelaheliheluheli";
+        assertEquals(jawaban,zonk.option("what is the dog name?"));
+    }
+    @Test
+    public void testMethodOption() {
+        String jawaban = "bandungjogjasolobalibali";
+        assertEquals(jawaban,zonk.option("capital city of indonesia?"));
     }
     @Test
     public void testMethodGantiSoal() {
-        assertEquals("indomie\napple\nnutrisari\ntoyota\napple",zonk.Gantisoal("mc donald phone number ?","apple"));
+        String option = "bandungjogjasolobalibali";
+        zonk.gantiSoal("capital city of indonesia?","bali");
+        assertEquals(option,zonk.option("capital city of indonesia?"));
     }
 
 }
