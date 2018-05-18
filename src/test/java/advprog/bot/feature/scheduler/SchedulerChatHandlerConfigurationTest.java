6b8743ef80aae82e24e7d0d941e434437ee08825
Kautsar Fadlillah <kautsarfadlillah@gmail.com>
2018-05-18 07:28:40 +0700

    Add Unit Tests (#60)

---
+++ src/test/java/advprog/bot/feature/scheduler/SchedulerChatHandlerConfigurationTest.java
@@ -0,0 +1,26 @@
+package advprog.bot.feature.scheduler;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import advprog.bot.BotController;
+import advprog.bot.line.LineChatHandler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SchedulerChatHandlerConfigurationTest {
+
+    @Test
+    public void testConstruction() {
+        BotController controller = mock(BotController.class);
+        LineChatHandler decoratedHandler = mock(LineChatHandler.class);
+        when(controller.getLineChatHandler()).thenReturn(decoratedHandler);
+        SchedulerChatHandlerConfiguration configuration = new SchedulerChatHandlerConfiguration();
+        SchedulerChatHandler schedulerChatHandler = configuration.schedulerChatHandler(controller);
+        assertEquals(decoratedHandler, schedulerChatHandler.getDecoratedLineChatHandler());
+    }
+
+}
---
+++ src/test/java/advprog/bot/feature/scheduler/SchedulerChatHandlerTest.java
@@ -0,0 +1,43 @@
+package advprog.bot.feature.scheduler;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import advprog.bot.ChatHandlerTestUtil;
+import advprog.bot.line.BaseChatHandler;
+import advprog.bot.line.LineChatHandler;
+
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.ImageMessageContent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SchedulerChatHandlerTest {
+    SchedulerChatHandler schedulerChatHandler;
+
+    @Before
+    public void setUp() {
+        schedulerChatHandler = new schedulerChatHandler(new BaseChatHandler());
+    }
+
+    @Test
+    public void testIgnoreNonTextMessageEvent() {
+        assertFalse(schedulerChatHandler.canHandleAudioMessage(null));
+        assertFalse(schedulerChatHandler.canHandleImageMessage(null));
+        assertFalse(schedulerChatHandler.canHandleStickerMessage(null));
+        assertFalse(schedulerChatHandler.canHandleLocationMessage(null));
+    }
+}
