a4fb09cd2b047ada1a46c5e27e00ec9553b6f9ea
regensa18 <regensa.cahyanto18@gmail.com>
2018-05-18 19:21:15 +0700

    added dependencies and added new test

--- src/test/java/advprog/bot/feature/enterkomputer/EnterKomputerChatHandlerTest.java
+++ src/test/java/advprog/bot/feature/enterkomputer/EnterKomputerChatHandlerTest.java
@@ -13,6 +13,7 @@ import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -40,4 +41,86 @@ public class EnterKomputerChatHandlerTest {
         );
         assertNotNull(enterkomputerChatHandler.handleTextMessageEvent(me, messages));
     }
+
+    @Test
+    public void testContextLoad() {
+        assertNotNull(enterkomputerChatHandler);
+    }
+
+    @Test
+    public void testHandleErrorCategoryEvent() throws IOException {
+        String msg = "/enterkomputer CATEGORY name";
+        List<Message> messages = new LinkedList<>();
+        List<TextMessage> expectedMessages = new LinkedList<>();
+        expectedMessages.add(new TextMessage("Sorry, we don't have the category"));
+        MessageEvent<TextMessageContent> me = ChatHandlerTestUtil.fakeMessageEvent(
+            "dsf", msg
+        );
+        assertEquals(expectedMessages, enterkomputerChatHandler.handleTextMessageEvent(me, messages));
+    }
+
+    @Test
+    public void testHandleNoResultEvent() throws IOException {
+        String msg = "/enterkomputer ssd thisisatest";
+        List<Message> messages = new LinkedList<>();
+        List<TextMessage> expectedMessages = new LinkedList<>();
+        expectedMessages.add(new TextMessage(
+            "Sorry, the product name is not available"));
+        MessageEvent<TextMessageContent> me = ChatHandlerTestUtil.fakeMessageEvent(
+            "dsf", msg
+        );
+        assertEquals(expectedMessages, enterkomputerChatHandler.handleTextMessageEvent(me, messages));
+    }
+
+    @Test
+    public void testHandleSuccessEvent() throws IOException {
+        String msg = "/enterkomputer ssd right";
+        List<Message> messages = new LinkedList<>();
+        List<TextMessage> expectedMessages = new LinkedList<>();
+        expectedMessages.add(new TextMessage(
+            "this is if the format is right"
+        ));
+        MessageEvent<TextMessageContent> me = ChatHandlerTestUtil.fakeMessageEvent(
+            "dsf", msg
+        );
+        assertEquals(expectedMessages, enterkomputerChatHandler.handleTextMessageEvent(me, messages));
+    }
+
+    @Test
+    public void testHandleNoCategoryEvent() throws IOException {
+        String msg = "/enterkomputer ";
+        List<Message> messages = new LinkedList<>();
+        List<TextMessage> expectedMessages = new LinkedList<>();
+        expectedMessages.add(new TextMessage(
+            "Please input the category name"
+        ));
+        MessageEvent<TextMessageContent> me = ChatHandlerTestUtil.fakeMessageEvent(
+            "dsf", msg
+        );
+        assertEquals(expectedMessages, enterkomputerChatHandler.handleTextMessageEvent(me, messages));
+    }
+
+    @Test
+    public void testHandleNoNameEvent() throws IOException {
+        String msg = "/enterkomputer ssd";
+        List<Message> messages = new LinkedList<>();
+        List<TextMessage> expectedMessages = new LinkedList<>();
+        expectedMessages.add(new TextMessage(
+            "Please input the name of the product"
+        ));
+        MessageEvent<TextMessageContent> me = ChatHandlerTestUtil.fakeMessageEvent(
+            "dsf", msg
+        );
+        assertEquals(expectedMessages, enterkomputerChatHandler.handleTextMessageEvent(me, messages));
+    }
+
+    @Test
+    public void testIgnoreNonTextMessageEvent() {
+        assertFalse(enterkomputerChatHandler.canHandleAudioMessage(null));
+        assertFalse(enterkomputerChatHandler.canHandleImageMessage(null));
+        assertFalse(enterkomputerChatHandler.canHandleStickerMessage(null));
+        assertFalse(enterkomputerChatHandler.canHandleLocationMessage(null));
+    }
+
+
 }
