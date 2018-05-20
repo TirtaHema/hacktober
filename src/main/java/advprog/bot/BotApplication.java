7401ebf19a5228a53a399879a32017e71e43760d
Yusuf <yfsholeh@gmail.com>
2018-05-20 20:59:50 +0700

    debug line 11

--- src/main/java/advprog/bot/BotApplication.java
+++ src/main/java/advprog/bot/BotApplication.java
@@ -4,9 +4,15 @@ import java.util.logging.Logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class BotApplication {
+
+    @RequestMapping(value = "/callback")
+    public String available() {
+        return "Spring in Action";
+    }
 
     private static final Logger LOGGER = Logger.getLogger(BotApplication.class.getName());
 
