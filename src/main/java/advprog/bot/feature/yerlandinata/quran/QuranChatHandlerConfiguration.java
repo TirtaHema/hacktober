085f4843051e49ad9bb9ba87c81ef56f54677579
Yudhistira Erlandinata <yerlandinata@gmail.com>
2018-05-23 03:47:40 +0700

    implement group chat

--- src/main/java/advprog/bot/feature/yerlandinata/quran/QuranChatHandlerConfiguration.java
+++ src/main/java/advprog/bot/feature/yerlandinata/quran/QuranChatHandlerConfiguration.java
@@ -5,6 +5,9 @@ import advprog.bot.feature.yerlandinata.quran.fetcher.AyatQuranFetcher;
 import advprog.bot.feature.yerlandinata.quran.fetcher.AyatQuranFetcherImpl;
 import advprog.bot.feature.yerlandinata.quran.fetcher.SurahQuranFetcher;
 import advprog.bot.feature.yerlandinata.quran.fetcher.SurahQuranFetcherImpl;
+import advprog.bot.feature.yerlandinata.quran.groupchat.QuranGroupChatHandler;
+import advprog.bot.feature.yerlandinata.quran.groupchat.service.GuessSurahService;
+import advprog.bot.feature.yerlandinata.quran.groupchat.service.GuessSurahServiceImpl;
 import advprog.bot.feature.yerlandinata.quran.privatechat.interactive.InteractivePrivateQuranChatHandler;
 import advprog.bot.feature.yerlandinata.quran.privatechat.interactive.service.InteractiveAyatFetcherService;
 import advprog.bot.feature.yerlandinata.quran.privatechat.interactive.service.InteractiveAyatFetcherServiceImpl;
@@ -29,6 +32,24 @@ public class QuranChatHandlerConfiguration {
         );
         controller.replaceLineChatHandler(handler);
         return handler;
+    }
+
+    @Bean
+    QuranGroupChatHandler quranGroupChatHandler(
+            BotController controller,
+            GuessSurahService guessSurahService
+    ) {
+        LineChatHandler currentChatHandler = controller.getLineChatHandler();
+        QuranGroupChatHandler quranGroupChatHandler = new QuranGroupChatHandler(
+                currentChatHandler, guessSurahService
+        );
+        controller.replaceLineChatHandler(quranGroupChatHandler);
+        return quranGroupChatHandler;
+    }
+
+    @Bean
+    GuessSurahService guessSurahService(AyatQuranFetcher ayatQuranFetcher) {
+        return new GuessSurahServiceImpl(ayatQuranFetcher);
     }
 
     @Bean
